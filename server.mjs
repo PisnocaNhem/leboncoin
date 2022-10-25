4843da0a20662fd9cff09c8b025bc5747b797740
Maskwelldev <102791393+Maskwelldev@users.noreply.github.com>
2022-10-25 14:06:54 +0100

    errors messages

--- server.mjs
+++ server.mjs
@@ -1,12 +1,18 @@
 import express from "express";
 import routes from "./routes/route.mjs";
 import path from "path";
-import {fileURLToPath} from "url";
+import {
+  fileURLToPath
+} from "url";
 
-import {createUser} from "./server/signup.js";
+import {
+  createUser
+} from "./server/signup.js";
+import { body, validationResult } from 'express-validator';
 
 
-const __filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(
+  import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
@@ -15,6 +21,7 @@ const PORT = process.env.PORT || 8082;
 // met en place le moteur de template
 app.set("view engine", "ejs");
 app.set("views", "views");
+
 // si on précise pas chemin, il va chercher dans le dossier node_modules
 app.use(express.static('public'));
 app.use("/", routes)
@@ -22,8 +29,15 @@ app.use("/", routes)
 // API Middlewares
 app.use(express.json()); // Parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
-app.post('/signUpCtrl', createUser); // crée un utilisateur
 
-app.listen(PORT, () => {							
+app.post('/signUp',
+  body('email').isEmail().withMessage('Email invalide'),
+  body('password').isLength({ min: 6 }).withMessage('Le mot de passe doit contenir au moins 6 caractères'),
+  body('name').isLength({ min: 3 }).withMessage('Le nom doit contenir au moins 3 caractères'),
+  body('name').isLength({ max: 20 }).withMessage('Le nom doit contenir au plus 20 caractères'),
+  createUser
+); // crée un utilisateur
+
+app.listen(PORT, () => {
   console.log('Notre server est en marche sur, ', PORT);
 });
