6af3934bbf342313342d170dc60654fb63c66b78
Etienne-Devillers <devillers.etienne80@gmail.com>
2022-10-25 12:31:08 +0200

    réparation de Nadir

--- server/product.js
+++ server/product.js
@@ -1,7 +1,16 @@
 import {dbConnect} from "../server/dbConnect.js";
 import {mysqlConnection} from "../server/dbConnect.js";
 
-export const getAll = (req, res) =>{
-
+export const getAll = (req, res) => {
+    console.log(req.body);
+    dbConnect();
     
+        const query = `SELECT * FROM announcements`;
+        mysqlConnection.query(query, (err, rows, fields) => {
+            if (!err) {
+                res.send(rows);
+            } else {
+                console.log(err);
+            }
+        });
 }
