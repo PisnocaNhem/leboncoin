f253ce52a94284549cf670ffba191568c170f5a9
Maskwelldev <102791393+Maskwelldev@users.noreply.github.com>
2022-10-25 11:13:33 +0100

    bite2

--- server.mjs
+++ server.mjs
@@ -2,21 +2,17 @@ import express from "express";
 import routes from "./routes/route.mjs";
 import path from "path";
 import {fileURLToPath} from "url";
-// const path = require('node:path')
 
-// require dbConnect.js file from server directory
 import {createUser} from "./server/signup.js";
 
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
-// process.env>PORT || 
 const app = express();
 const PORT = process.env.PORT || 8082;
 
-// set the view engine to ejs
+// met en place le moteur de template
 app.set("view engine", "ejs");
 app.set("views", "views");
 // si on précise pas chemin, il va chercher dans le dossier node_modules
@@ -26,9 +22,7 @@ app.use("/", routes)
 // API Middlewares
 app.use(express.json()); // Parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
-
-// API Routes
-app.post('/signUpCtrl', createUser);
+app.post('/signUpCtrl', createUser); // crée un utilisateur
 
 app.listen(PORT, () => {							
   console.log('Notre server est en marche sur, ', PORT);
--- server/signup.js
+++ server/signup.js
@@ -1,19 +1,19 @@
 import {dbConnect} from "../server/dbConnect.js";
 import {mysqlConnection} from "../server/dbConnect.js";
+import * as bcrypt from 'bcrypt';
 // création d'un utilisateur en base de données
 export const createUser = (req, res) => {
     console.log(req.body);
+    const passwordHash = bcrypt.hashSync(req.body.password, 10);
     dbConnect();
     if (req.body.email && req.body.username && req.body.password && req.body.password === req.body.password2) {
-        // on filtre les données reçues
         const {
             name,
             email,
-            password,
             role = 1,
-            newletter = 0,
+            newletter = 1,
         } = req.body;
-        const query = `INSERT INTO users (name, mail, password, role, newsletter) VALUES ('${name}', '${email}', '${password}', '${role}', '${newletter}')`;
+        const query = `INSERT INTO users (name, mail, password, role, newsletter) VALUES ('${name}', '${email}', '${passwordHash}', '${role}', '${newletter}')`;
         mysqlConnection.query(query, (err, rows, fields) => {
             if (!err) {
                 res.send(rows);
@@ -21,7 +21,11 @@ export const createUser = (req, res) => {
                 if(req.body.password !== req.body.password2){
                     res.send("Les mots de passe ne correspondent pas");
                 }
-                // console.log(err);
+                // si les champs sont vides
+                if(req.body.email === "" || req.body.username === "" || req.body.password === ""){
+                    res.send("Veuillez remplir tous les champs");
+                }
+                console.log(err);
             }
         });
     } else {
