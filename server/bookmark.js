54122d6bd13be1c10308775a61238c1a37621d4c
Pisnoca Nhem <pisnoca_nhem@hotmail.fr>
2022-10-26 15:06:56 +0200

    fin favori

--- server/bookmark.js
+++ server/bookmark.js
@@ -7,7 +7,6 @@ export const getBookMark = (req, res) =>
     const query = `SELECT * FROM bookmark INNER JOIN announcements ON bookmark.id_annoucement = announcements.id_announcement;`
     mysqlConnection.query(query, (err, rows, fields) => {
         if (!err) {
-            console.log(rows);
             res.render("bookmark", {rows:rows})
         } else {
             console.log('on passe dans le else')
